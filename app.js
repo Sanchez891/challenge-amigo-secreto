635183921571656cc86bfb57405c263ba5e11fe5
Sanchez891 <sanchezjua929@gmail.com>
2025-07-29 01:28:49 -0300

    agregue la funcion de listar a medida que se agregan los amigos

--- app.js
+++ app.js
@@ -5,14 +5,26 @@ let lista_amigos = [];
 function agregarAmigo(){
     let amigoAgregado = document.getElementById("amigo").value;
     if (amigoAgregado == " "){
-        alert("no ingresar campos vacios, intentelo nuevamente.")
-        limpiarCaja()
+        alert("no ingresar campos vacios, intentelo nuevamente.");
+        limpiarCaja();
     }else{
-        lista_amigos.push(amigoAgregado)
-        limpiarCaja()
+        lista_amigos.push(amigoAgregado);
+        mostrarLista();
+        limpiarCaja();
     }
 }
 
 function limpiarCaja(){
     document.querySelector("#amigo").value = " ";
+}
+
+//funcion para mostrar la lista de amigos en la pantalla
+function mostrarLista(){
+    var listaImprimida = document.getElementById("listaAmigos");
+    listaImprimida.innerHTML = " ";
+    for (let i = 0; i < lista_amigos.length; i++){
+        var amigo = document.createElement("li");
+        amigo.textContent = lista_amigos[i];
+        listaImprimida.appendChild(amigo);
+    }
 }
