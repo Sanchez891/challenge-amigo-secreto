de8444cf86d753c21a459021f07c3b418ad5a774
Sanchez891 <sanchezjua929@gmail.com>
2025-07-28 22:18:05 -0300

    agrege la funcionalidad de agregarAmigo al app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//declaracion de variables
+let lista_amigos = [];
+
+//funcionalidad para agregar a la lista
+function agregarAmigo(){
+    let amigoAgregado = document.getElementById("amigo").value;
+    if (amigoAgregado == " "){
+        alert("no ingresar campos vacios, intentelo nuevamente.")
+        limpiarCaja()
+    }else{
+        lista_amigos.push(amigoAgregado)
+        limpiarCaja()
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = " ";
+}
